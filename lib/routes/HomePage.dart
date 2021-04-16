371bdd5615145c207a01e60c73d9150b048f2c7f
Polpy <emailparaoedu@gmail.com>
2021-04-16 18:47:36 +0100

    trying stuff @  homepage

--- lib/routes/HomePage.dart
+++ lib/routes/HomePage.dart
@@ -1,73 +1,53 @@
-import 'dart:math';
 import 'package:flutter/material.dart';
-import 'package:a3team/sidebar/SideBarLayout.dart';
-import 'package:flutter/rendering.dart';
-
-String getRandomImage() {
-  List<String> image2show = ['static/images/polo1.jpg', 'static/images/polo2.jpg','static/images/polo3.jpg'];
-  final random = new Random();
-  var i = random.nextInt(image2show.length);
-
-  return image2show[i];
-}
-
+import 'package:a3team/components/SideBar.dart';
 
 class HomePage extends StatefulWidget {
-  HomePage({Key key, this.title}) : super(key: key);
-  final String title;
+  HomePage({Key? key}) : super(key: key);
 
   @override
-  _HomePageState createState() => _HomePageState();
-
+  _MyHomePageState createState() => _MyHomePageState();
 }
 
-class _HomePageState extends State<HomePage> {
+class _MyHomePageState extends State<HomePage> {
+
+
   bool _pinned = true;
   bool _snap = false;
   bool _floating = false;
-
-
+  int favcount = 1;
+  //GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      key: GlobalKey<ScaffoldState>(),
+      drawer: SideBar(),
       body: CustomScrollView(
         slivers: <Widget>[
           SliverAppBar(
-            elevation: 5.0,
+            expandedHeight: 280,
             pinned: _pinned,
-            snap: _snap,
             floating: _floating,
-            expandedHeight: 390.0,
-
-            leading: GestureDetector(
-              onTap: () { },
-              child: Icon(
-                Icons.menu,  // add custom icons also
-                size: 30,
-              ),
-            ),
-
             flexibleSpace: FlexibleSpaceBar(
-              background: Image(
-                fit: BoxFit.cover,
-                image:
-                AssetImage(getRandomImage()),
+              title: Text("Hygge",
+              style: TextStyle(color: Colors.white, fontSize: 35),
               ),
+
             ),
           ),
-          SliverList(
+          SliverFixedExtentList(
+            itemExtent: 150,
             //Depois escrevemos aqui o conteudo de baixo.
-            delegate: SliverChildBuilderDelegate(
-                  (BuildContext context, int index) {
-                return Container(
-                  color: index.isOdd ? Colors.white : Colors.black12,
-                  height: 100.0,
-                  child: Center(
-                    child: Text('$index', textScaleFactor: 5),
+            delegate: SliverChildBuilderDelegate((BuildContext context, int index) {
+              return Container(
+                color: Colors.white,
+                child: Center(
+                  child: Text(
+                    "Favourites",
+                    style: TextStyle(color: Color(0xFF9a33b6), fontSize: 30),
                   ),
-                );
-              },
-              childCount: 20,
+                ),
+              );
+              },childCount: favcount,
             ),
           ),
         ],
